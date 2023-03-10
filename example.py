431e730c9f74f9da8e988960e73e085ed194b138
mitumh3 <92108351+mitumh3@users.noreply.github.com>
2023-03-10 23:23:26 +0700

    Update example.py

--- example.py
+++ example.py
@@ -40,97 +40,100 @@ async def bot():
     while True:
 #StartTheBot
         bot = await TelegramClient(None, api_id, api_hash).start(bot_token=botToken)
+        bot_info = await bot.get_me()
+        bot_id = bot_info.id
         @bot.on(events.NewMessage)
         async def event_handler(event):
             user = event.sender_id
             message = event.raw_text
-            if not os.path.exists(f"{user}_session.json"):
-                data = {"session": 1}
-                with open(f"{user}_session.json", 'w') as f:
-                    json.dump(data, f)
-            filename, prompt, num_tokens = start_and_check(message, user)
-                # Get response from openai and send to user
-            respose = get_response(prompt, filename)
-            await event.respond(f"{respose}\n\n(used {num_tokens} tokens)")
+            if user != bot_id:
+                if not os.path.exists(f"{user}_session.json"):
+                    data = {"session": 1}
+                    with open(f"{user}_session.json", 'w') as f:
+                        json.dump(data, f)
+                filename, prompt, num_tokens = start_and_check(message, user)
+                    # Get response from openai and send to user
+                respose = get_response(prompt, filename)
+                await event.respond(f"{respose}\n\n(used {num_tokens} tokens)")
 
-        def start_and_check(message, user):
-            while True:
-                with open(f"{user}_session.json", 'r') as f:
-                    file_num=json.load(f)['session']
-                filename = f'chats/{user}_{file_num}.json'
-                    # Create .json file in case of new chat
-                if not os.path.exists(filename):
-                    data = {
+            def start_and_check(message, user):
+                while True:
+                    with open(f"{user}_session.json", 'r') as f:
+                        file_num=json.load(f)['session']
+                    filename = f'chats/{user}_{file_num}.json'
+                        # Create .json file in case of new chat
+                    if not os.path.exists(filename):
+                        data = {
+                                "messages": [{
+                                            "role": "system",
+                                            "content": "You are a large AI language model. you know everything. Your job is to provide solutions / suggestion to problems. Your name is Minnion. If found mistakes in English of question, rewrite it before giving responses. Ask for more details if needed. You were created by @thisaintminh."
+                                            }],
+                                "num_tokens": 0
+                                }
+                        with open(filename, 'w') as f:
+                            json.dump(data, f, indent=4)
+                    with open(filename, 'r') as f:
+                        data = json.load(f)
+                    prompt = []
+                    for item in data['messages']:
+                        prompt.append(item)
+                        
+                    num_tokens=data["num_tokens"]
+                    if num_tokens > 4000:
+                        file_num += 1
+                        data = {"session": file_num}
+                        with open(f"{user}_session.json", 'w') as f:
+                            json.dump(data, f)
+                        bot.send_message('thisaintminh', f"{num_tokens} exceeds 4096, creating new chat")
+                        prompt.append({
+                            "role": "user",
+                            "content": "summarize this conversation"
+                        })
+                        completion = openai.ChatCompletion.create(
+                                                                    model='gpt-3.5-turbo',
+                                                                    messages=prompt
+                                                                )
+                        response = completion.choices[0].message.content
+                        num_tokens = completion.usage.total_tokens
+                        data = {
                             "messages": [{
                                         "role": "system",
                                         "content": "You are a large AI language model. you know everything. Your job is to provide solutions / suggestion to problems. Your name is Minnion. If found mistakes in English of question, rewrite it before giving responses. Ask for more details if needed. You were created by @thisaintminh."
+                                        },
+                                        {
+                                        "role": "system",
+                                        "content": response
                                         }],
-                            "num_tokens": 0
-                            }
-                    with open(filename, 'w') as f:
-                        json.dump(data, f, indent=4)
-                with open(filename, 'r') as f:
-                    data = json.load(f)
-                prompt = []
-                for item in data['messages']:
-                    prompt.append(item)
-                    
-                num_tokens=data["num_tokens"]
-                if num_tokens > 4000:
-                    file_num += 1
-                    data = {"session": file_num}
-                    with open(f"{user}_session.json", 'w') as f:
-                        json.dump(data, f)
-                    bot.send_message('thisaintminh', f"{num_tokens} exceeds 4096, creating new chat")
-                    prompt.append({
-                        "role": "user",
-                        "content": "summarize this conversation"
+                            "num_tokens": num_tokens
+                                }
+                        with open(filename, 'w') as f:
+                            json.dump(data, f, indent=4)
+                        continue
+                    else: break
+
+                prompt.append({
+                                "role": "user",
+                                "content": message
+                            }) 
+                return filename, prompt, num_tokens
+
+
+            def get_response(prompt, filename):
+                completion = openai.ChatCompletion.create(
+                    model='gpt-3.5-turbo',
+                    messages=prompt
+                )
+                response = completion.choices[0].message.content
+                num_tokens = completion.usage.total_tokens
+                prompt.append(
+                    {
+                        "role": "assistant",
+                        "content": response
                     })
-                    completion = openai.ChatCompletion.create(
-                                                                model='gpt-3.5-turbo',
-                                                                messages=prompt
-                                                            )
-                    response = completion.choices[0].message.content
-                    num_tokens = completion.usage.total_tokens
-                    data = {
-                        "messages": [{
-                                    "role": "system",
-                                    "content": "You are a large AI language model. you know everything. Your job is to provide solutions / suggestion to problems. Your name is Minnion. If found mistakes in English of question, rewrite it before giving responses. Ask for more details if needed. You were created by @thisaintminh."
-                                    },
-                                    {
-                                    "role": "system",
-                                    "content": response
-                                    }],
-                        "num_tokens": num_tokens
-                            }
-                    with open(filename, 'w') as f:
-                        json.dump(data, f, indent=4)
-                    continue
-                else: break
-
-            prompt.append({
-                            "role": "user",
-                            "content": message
-                        }) 
-            return filename, prompt, num_tokens
-
-
-        def get_response(prompt, filename):
-            completion = openai.ChatCompletion.create(
-                model='gpt-3.5-turbo',
-                messages=prompt
-            )
-            response = completion.choices[0].message.content
-            num_tokens = completion.usage.total_tokens
-            prompt.append(
-                {
-                    "role": "assistant",
-                    "content": response
-                })
-            data= {"messages":prompt, "num_tokens":num_tokens}
-            with open(filename, 'w') as f:
-                json.dump(data, f, indent=4)
-            return response
+                data= {"messages":prompt, "num_tokens":num_tokens}
+                with open(filename, 'w') as f:
+                    json.dump(data, f, indent=4)
+                return response
 
         await bot.run_until_disconnected()
 
@@ -152,7 +155,7 @@ def health_check():
     return {f"Bot is online {Minversion}"}
 
 
-#Minnion
+#Minnion run
 if __name__ == '__main__':
     HOST=os.getenv("HOST", "0.0.0.0")
     PORT=os.getenv("PORT", "8080")
